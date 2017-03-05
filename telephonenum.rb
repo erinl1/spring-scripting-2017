63813acd0b28f29218b5c90fde29671acdfbbbd8
erinl1 <erin.lee.ny@gmail.com>
2017-03-05 10:05:12 -0500

    @rickyc
    Ok! Should I merge it in now?

--- telephonenum.rb
+++ telephonenum.rb
@@ -2,28 +2,28 @@ require "csv"
 ary = CSV.read("telephonenumbers.csv")
 #for each broken phone #
 CSV.open("correctnumbers.csv","wb") do |csv|
-  changeNum = ary.map do |i|
+  change_num = ary.map do |i|
     #make a new array to add the correct num
-    newAry = []
+    new_ary = []
     #get rid of spaces and stuff
-    letterArray = i[0].scan /\w/
+    letter_array = i[0].scan /\w/
     #for each digit
-    letterArray.each do |x|
+    letter_array.each do |x|
       #check if each digit is an actual num
-      checkDigit = x =~ /\A\d+\z/ ? true : false
+      check_digit = x =~ /\A\d+\z/ ? true : false
       #if it is a digit, add to the ary
-      if checkDigit == true
-        newAry.push(x)
+      if check_digit == true
+        new_ary.push(x)
       end
     end
     #the 1- is not needed
-    if newAry.length == 11
+    if new_ary.length == 11
       # adds into file
-      csv << ["#{newAry[0]}-#{newAry[1]}#{newAry[2]}#{newAry[3]}-#{newAry[4]}#{newAry[5]}#{newAry[6]}-#{newAry[7]}#{newAry[8]}#{newAry[9]}#{newAry[10]}"]
+      csv << ["#{new_ary[0]}-#{new_ary[1..3].join}-#{new_ary[4..6].join}-#{new_ary[7..10].join}"]
       #the 1- is needed
-    elsif newAry.length == 10
+    elsif new_ary.length == 10
       #adds into file
-      csv << ["1-#{newAry[0]}#{newAry[1]}#{newAry[2]}-#{newAry[3]}#{newAry[4]}#{newAry[5]}-#{newAry[6]}#{newAry[7]}#{newAry[8]}#{newAry[9]}"]
+      csv << ["1-#{new_ary[0..2].join}-#{new_ary[3..5].join}-#{new_ary[6..9].join}"]
     end
   end
 end

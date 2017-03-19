3ebfcd2e8f0ef3c0dbc3a9332836e562167ef620
erinl1 <erin.lee.ny@gmail.com>
2017-03-19 13:38:54 -0400

    @rickyc I made it worse

--- mapdata.rb
+++ mapdata.rb
@@ -1,18 +1,19 @@
 require "csv"
 ary = CSV.read("header.csv")
 array = CSV.read("exampledata.csv")
-#puts array.inspect
+#puts array[0][0]
 library = []
 #CSV.open("exampledata.csv", "wb") do |row|
 #data_array = array.map do |value|
-array.each do |key|
-  puts key[0]
-  array[1].each do |value|
-  #ary.each do |key|
-  #  puts value[0].inspect
-    #  puts value.inspect
-    x = {}
-    x.store(:hello, value)
+
+  #ary.each do |value|
+#puts value
+x = {}
+  (0..12).each do |index|
+    key = array[0][index]
+    x.store(key.to_sym, key)
+    puts x
+    #  puts value[i]
+    end
     #puts x[:hello]
-  end
-end
+#puts data

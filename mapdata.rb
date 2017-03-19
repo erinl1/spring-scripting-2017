b2451f92a7423d252cc84e9ff3e9bcc2401ce359
erinl1 <erin.lee.ny@gmail.com>
2017-03-19 00:37:06 -0400

    Weird

--- mapdata.rb
+++ mapdata.rb
@@ -1,15 +1,18 @@
 require "csv"
 ary = CSV.read("header.csv")
 array = CSV.read("exampledata.csv")
+#puts array.inspect
 library = []
 #CSV.open("exampledata.csv", "wb") do |row|
 #data_array = array.map do |value|
-array.each do |value|
+array[0].each do |key|
+  puts key.inspect
+  array[1].each do |value|
   #ary.each do |key|
   #  puts value[0].inspect
-    #  puts value
+      puts value.inspect
     x = {}
-    x.store(:hello, value[0])
-    puts x.inspect
-#  end
+    x.store(key.to_sym, value)
+    #puts x.inspect
+  end
 end

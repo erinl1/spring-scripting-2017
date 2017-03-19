39dd36a2a1cd3dbaf74d16c856d96a7113553841
erinl1 <erin.lee.ny@gmail.com>
2017-03-19 00:17:11 -0400

    Help by Beatles @rickyc
    How do I just get the values instead of both key and value in one param?

--- mapdata.rb
+++ mapdata.rb
@@ -1,16 +1,15 @@
 require "csv"
 ary = CSV.read("header.csv")
-#array = CSV.read("exampledata.csv")
+array = CSV.read("exampledata.csv")
 library = []
 #CSV.open("exampledata.csv", "wb") do |row|
 #data_array = array.map do |value|
-  ary.each do |key|
-    data_array = array.map do |value|
-  #  puts value
-  puts key[0]
-
-  x = {}
-  #x.store(key.to_s, value)
-  #puts x.inspect
-  end
+array.each do |value|
+  #ary.each do |key|
+  #  puts value[0].inspect
+    #  puts value
+    x = {}
+    x.store(:hello, value[0])
+    puts x.inspect
+#  end
 end

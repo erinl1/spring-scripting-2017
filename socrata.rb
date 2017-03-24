d0dd9cbc322c52f4589e36336dd83ea58978d04f
erinl1 <erin.lee.ny@gmail.com>
2017-03-24 16:51:16 -0400

    Fads

--- socrata.rb
+++ socrata.rb
@@ -1,2 +1,4 @@
-client = SODA::Client.new({:domain => "explore.data.gov", :app_token => "w4imPv49yhvBlkOUukeVAUZeD"})
+require 'soda/client'
+client = SODA::Client.new({:domain => "socrata", :app_token => "w4imPv49yhvBlkOUukeVAUZeD"})
 response = client.get("644b-gaut", {"$limit" => 1, :namelast => "WINFREY", :namefirst => "OPRAH"})
+puts response

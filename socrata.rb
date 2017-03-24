7f218ca00e048e3ef989d9e19bfe31332ce7af5e
erinl1 <erin.lee.ny@gmail.com>
2017-03-24 13:54:37 -0400

    Progress

---
+++ socrata.rb
@@ -0,0 +1,2 @@
+client = SODA::Client.new({:domain => "explore.data.gov", :app_token => "w4imPv49yhvBlkOUukeVAUZeD"})
+response = client.get("644b-gaut", {"$limit" => 1, :namelast => "WINFREY", :namefirst => "OPRAH"})

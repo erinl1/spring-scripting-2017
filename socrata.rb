3962fd8353b7a6baea554eafd0aa5e1655f02750
erinl1 <erin.lee.ny@gmail.com>
2017-03-24 16:58:32 -0400

    I don't get it @rickyc :(

--- socrata.rb
+++ socrata.rb
@@ -1,4 +1,4 @@
 require 'soda/client'
-client = SODA::Client.new({:domain => "socrata", :app_token => "w4imPv49yhvBlkOUukeVAUZeD"})
+client = SODA::Client.new({:domain => "explore.data.gov", :app_token => "w4imPv49yhvBlkOUukeVAUZeD"})
 response = client.get("644b-gaut", {"$limit" => 1, :namelast => "WINFREY", :namefirst => "OPRAH"})
 puts response

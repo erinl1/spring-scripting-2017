b9612b8dd146c6c4dbd0581892234f28654cf3b5
erinl1 <erin.lee.ny@gmail.com>
2017-03-31 19:03:09 -0400

    Config son

--- gdexample.rb
+++ gdexample.rb
@@ -1,5 +1,7 @@
 require "google_drive"
 require 'soda/client'
+require "dotenv"
+Dotenv.load
 client = SODA::Client.new({domain: "data.cityofnewyork.us", app_token: "w4imPv49yhvBlkOUukeVAUZeD"})
 response = client.get("feuq-due4")
 puts response[0].values.inspect
@@ -11,7 +13,7 @@ session = GoogleDrive::Session.from_config("config.json")
 # First worksheet of
 # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
 # Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
-ws = session.spreadsheet_by_key("1tOatwbeCtqw67v7dE1s-FjE7SOJQ-qxthn04nMnauFY").worksheets[0]
+ws = session.spreadsheet_by_key(ENV["GOOGLE_INIT"]).worksheets[0]
 
 # Gets content of A2 cell.
 p ws[1, 1]  = "NY OPENDATA LIBRARIES"#==> "hoge"
@@ -21,12 +23,14 @@ p ws[1, 1]  = "NY OPENDATA LIBRARIES"#==> "hoge"
 (1..response.first.keys.length).each do |param|
   ws[2,param+1] = response.first.keys[param-1]
 end
+
 (0..response.length-1).each do |i|
     array = response[i].values
     (0..array.length).each do |data|
     ws[i+3, data+2] = array[data]
 end
 end
+
 (0..300).each do |me|
   ws[1,me+1] = " "
 end

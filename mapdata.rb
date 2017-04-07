a152f0a1937d19e1a736ab7c58d0ecbd6c4edfd1
erinl1 <erin.lee.ny@gmail.com>
2017-04-07 16:14:57 -0400

    Csv edit

--- mapdata.rb
+++ mapdata.rb
@@ -1,19 +1,54 @@
 require "csv"
-ary = CSV.read("header.csv")
-array = CSV.read("exampledata.csv")
-#puts array[0][0]
+require "json"
+sub_csv = CSV.read("subway.csv")
+example_data = CSV.read("library.csv")
+example = [[["gogsok":"sgfd"],["hello":"sdgg"]]]
+opendata = []
+lib_keys = []
 library = []
-#CSV.open("exampledata.csv", "wb") do |row|
-#data_array = array.map do |value|
+example_data[0].each do |key|
+  lib_keys.push(key)
+end
 
-  #ary.each do |value|
-#puts value
-x = {}
-  (0..12).each do |index|
-    key = array[0][index]
-    x.store(key.to_sym, key)
-    puts x
-    #  puts value[i]
+my_array = []
+(1..example_data.length-1).each do |row|
+  my_obj = {}
+  index = 0
+  example_data[row].each do |value|
+    if lib_keys[index]
+      my_obj[lib_keys[index].to_sym] = value
     end
-    #puts x[:hello]
-#puts data
+    index = index + 1
+  end
+  library.push(my_obj)
+end
+
+
+sub_keys = []
+subway = []
+sub_csv[0].each do |key|
+  sub_keys.push(key)
+end
+my_array = []
+(1..sub_csv.length-1).each do |row|
+  my_obj = {}
+  index = 0
+  sub_csv[row].each do |value|
+    if sub_keys[index]
+      my_obj[sub_keys[index].to_sym] = value
+    end
+    index = index + 1
+  end
+  subway.push(my_obj)
+end
+
+opendata.push(subway)
+opendata.push(library)
+json = JSON.parse(opendata.to_json)
+#puts json
+json.each do |row|
+  puts row
+end
+File.open("opendata.json", "wb") do |file|
+  file.write(json)
+end
---
+++ opendata.js
@@ -0,0 +1,2 @@
+$.get('opendata.json', function(data) {    
+});

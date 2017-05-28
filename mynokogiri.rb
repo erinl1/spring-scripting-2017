ea354b72bcc8e08584d41fbda2355b497f368214
erinl1 <erin.lee.ny@gmail.com>
2017-05-28 08:05:48 -0400

    edits

--- mynokogiri.rb
+++ mynokogiri.rb
@@ -4,6 +4,9 @@ require 'nokogiri'
 require 'json'
 doc = Nokogiri::HTML(open("https://www.urbanoutfitters.com/new-music-tech"))
 title = doc.css('span[itemprop ="name"]')
+image = doc.css('span[itemprop ="image"]')
+url = image[0].css('meta[itemprop ="url"]')
+puts url[0].("content")
 hash = []
 [0..title.length-1].each do |x|
   row = title[x].text[13...-9]
@@ -12,10 +15,6 @@ hash = []
   #puts hash[:item][x].inspect
 end
   json = JSON.parse(hash.to_json)
-  puts json
-  json.each do |row|
-    puts row
-  end
   File.open("tech.json", "wb") do |file|
     file.write(json)
   end

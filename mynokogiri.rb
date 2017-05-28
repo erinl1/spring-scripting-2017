67e51433c36724c232d7091cc66b62addbbe7bcb
erinl1 <erin.lee.ny@gmail.com>
2017-05-28 01:06:31 -0400

    Web scraping

--- mynokogiri.rb
+++ mynokogiri.rb
@@ -1,4 +1,21 @@
 require 'open-uri'
+require 'rubygems'
 require 'nokogiri'
-doc = Nokogiri::HTML(open("https://my.pottermore.com"))
-puts doc.css(".header__menu .link-text .innerText")
+require 'json'
+doc = Nokogiri::HTML(open("https://www.urbanoutfitters.com/new-music-tech"))
+title = doc.css('span[itemprop ="name"]')
+hash = []
+[0..title.length-1].each do |x|
+  row = title[x].text[13...-9]
+  hash.push(row)#puts row
+#  puts hash[:item][x]
+  #puts hash[:item][x].inspect
+end
+  json = JSON.parse(hash.to_json)
+  puts json
+  json.each do |row|
+    puts row
+  end
+  File.open("tech.json", "wb") do |file|
+    file.write(json)
+  end

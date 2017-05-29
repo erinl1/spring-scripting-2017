b0ddbc7f6c17166d86eb8e29d1aa51d086d9a08f
erinl1 <erin.lee.ny@gmail.com>
2017-05-28 22:22:17 -0400

    Css scraping

--- mynokogiri.rb
+++ mynokogiri.rb
@@ -3,18 +3,23 @@ require 'rubygems'
 require 'nokogiri'
 require 'json'
 doc = Nokogiri::HTML(open("https://www.urbanoutfitters.com/new-music-tech"))
-title = doc.css('span[itemprop ="name"]')
+name = doc.css('span[itemprop ="name"]')
 image = doc.css('span[itemprop ="image"]')
-url = image[0].css('meta[itemprop ="url"]')
-puts url[0].("content")
-hash = []
-[0..title.length-1].each do |x|
-  row = title[x].text[13...-9]
-  hash.push(row)#puts row
-#  puts hash[:item][x]
-  #puts hash[:item][x].inspect
+url = image.css('meta[itemprop ="url"]')
+text_array = []
+img_array = []
+urban_hash = {}
+puts title[0].text[13...-9]
+0.upto(url.length-1) do |x|
+  img_array.push(url[x]["content"])
 end
-  json = JSON.parse(hash.to_json)
-  File.open("tech.json", "wb") do |file|
-    file.write(json)
-  end
+2.upto(title.length-1) do |x|
+  text = name[x].text[13...-9]
+  text_array.push(text)
+end
+urban_hash["text"] = text_array
+urban_hash["img"] = img_array
+json = urban_hash.to_json
+File.open("urban.json", "a+") do |file|
+  file.write(json)
+end

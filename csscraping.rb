93a633f5b758ce2b77de46d23f3e7aba5049276c
erinl1 <erin.lee.ny@gmail.com>
2017-05-28 22:51:41 -0400

    Hw 3

---
+++ csscraping.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require 'rubygems'
+require 'nokogiri'
+require 'json'
+doc = Nokogiri::HTML(open("https://www.urbanoutfitters.com/new-music-tech"))
+name = doc.css('span[itemprop ="name"]')
+image = doc.css('span[itemprop ="image"]')
+url = image.css('meta[itemprop ="url"]')
+text_array = []
+img_array = []
+urban_hash = {}
+puts name[0].text[13...-9]
+0.upto(url.length-1) do |x|
+  img_array.push(url[x]["content"])
+end
+2.upto(name.length-1) do |x|
+  text = name[x].text[13...-9]
+  text_array.push(text)
+end
+urban_hash["text"] = text_array
+urban_hash["img"] = img_array
+json = urban_hash.to_json
+File.open("urban.json", "a+") do |file|
+  file.write(json)
+end

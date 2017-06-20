e6fa937d01875aabdbcd2fdf2673a22b340f772c
erinl1 <erin.lee.ny@gmail.com>
2017-06-19 21:43:05 -0400

    Google alerts start

--- googlealerts.rb
+++ googlealerts.rb
@@ -1,6 +1,2 @@
-require 'open-uri'
-require 'rubygems'
-require 'nokogiri'
-require 'json'
-doc = Nokogiri::HTML(open("https://www.google.com/alerts"))
-puts doc
+require 'launchy'
+Launchy.open("https://www.google.com/alerts")

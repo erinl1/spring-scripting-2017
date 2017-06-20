70364ff873376b30cbda3c1971e43df38bc5644b
erinl1 <erin.lee.ny@gmail.com>
2017-06-19 21:45:23 -0400

    conflicts

--- newyorktimes.rb
+++
@@ -1,5 +0,0 @@
-require 'open-uri'
-require 'rubygems'
-require 'nokogiri'
-require 'json'
-doc = Nokogiri::HTML(open('https://www.nytimes.com/section/technology?WT.nav=page&action=click&contentCollection=Tech&module=HPMiniNav&pgtype=Homepage&region=TopBar'))
